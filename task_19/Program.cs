3ac220edaa5926cff9465148be2fc29ca3481c68
Angelina <chulakovaav@yandex.ru>
2022-09-23 11:50:55 +0300

    added answers

---
+++ task_19/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+
+//Решение задачи без применения массива и для пятизначных чисел:
+
+/*
+Console.Write("Введите пятизначное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x >= 99999 || x < 10000)
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
+else
+{
+    int y1 = x / 10000;
+    int y2 = (x - y1 * 10000) / 1000;
+    int y3 = (x - y1 * 10000 - y2 * 1000) / 100;
+    int y4 = (x - y1 * 10000 - y2 * 1000 - y3 * 100) / 10;
+    int y5 = x % 10;
+    if (y1 == y5 && y2 == y4)
+    {
+        Console.WriteLine($"Число {x} - палиндром!");
+    }
+    else
+    {
+        Console.WriteLine($"Число {x} - не палиндром!");
+    }
+}
+*/
+
+
+//Решение задачи для любых чисел с помощью массива:
+
+/*
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+int y = x;
+while (y > 0)  //цикл, чтоб узнать количество цифр в числе
+{
+    y = y/10;
+    index++;
+}
+
+int [] array;  // ввели значения цифр в массив (в обратном порядке)
+array = new int[index]; 
+int i = 0;
+int l = x;
+while (i < index) 
+{
+    array[i] = l % 10;
+    l /= 10;
+    i++;
+}
+
+int ind = 0;   // Сравниваем значение начала массива с его концом
+int sorted_size = 1;
+if (array[ind] == array[index-sorted_size])
+{
+    while (i < index - sorted_size )
+    {
+        ind++;
+        sorted_size++;
+    }
+    Console.WriteLine($"Число {x} -  палиндром!");
+}
+else 
+{
+    Console.WriteLine($"Число {x} - не палиндром!");
+}
+*/
+
---
+++ task_21/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+Console.Clear();
+Console.WriteLine("Найдем расстояние между двумя точками.");
+Console.Write("Введите первую координату точки А: ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую координату точки А: ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третью координату точки А: ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую координату точки В: ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую координату точки В: ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третью координату точки В: ");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Точка А ({X1},{Y1},{Z1}); Точка В ({X2},{Y2},{Z2}).");
+double d = Math.Sqrt(Math.Pow((X2-X1), 2) + Math.Pow((Y2-Y1), 2) + Math.Pow((Z2 - Z1), 2));
+Console.WriteLine($"Расстояние между точками = {Math.Round(d, 2)}.");
